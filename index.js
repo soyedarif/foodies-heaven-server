37baac57e3092c700889e5fd841790bd8fec5a80
soyedarif <soyedariff@gmail.com>
2023-05-02 14:34:56 +0600

    chefs data imported

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express= require('express')
+const cors= require('cors')
 const app=express()
 const port=process.env.PORT || 5000;
 
+app.use(cors())
+
+const chefs=require('./data/chefs.json')
+
 app.get('/',(req,res)=>{
     res.send('kire dada')
+})
+
+app.get('/chefs',(req,res)=>{
+    res.send(chefs)
 })
 
 app.listen(port)
