f2b413cb25070744e6582f4a727e405c21fd3a03
soyedarif <soyedariff@gmail.com>
2023-05-02 14:36:38 +0600

    recipies data imported

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const port=process.env.PORT || 5000;
 
 app.use(cors())
 
-const chefs=require('./data/chefs.json')
+const chefs=require('./data/chefs.json');
+const recipies=require('./data/recipies.json')
 
 app.get('/',(req,res)=>{
     res.send('kire dada')
@@ -13,6 +14,10 @@ app.get('/',(req,res)=>{
 
 app.get('/chefs',(req,res)=>{
     res.send(chefs)
+})
+
+app.get('/recipies',(req,res)=>{
+    res.send(recipies)
 })
 
 app.listen(port)
